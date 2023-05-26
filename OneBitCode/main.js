b0c47a686f019979cf32244589f55779e864fc63
Nicro01 <110069146+Nicro01@users.noreply.github.com>
2023-05-26 12:09:55 -0300

    Update main.js

--- OneBitCode/main.js
+++ OneBitCode/main.js
@@ -1,11 +1,11 @@
 const buttons = document.querySelectorAll(".button");
 const display = document.getElementById("tela");
-var keys1 = document.querySelector(".teclas");
-var keys2 = document.querySelector(".teclas-2");
+const keys1 = document.querySelector(".teclas");
+const keys2 = document.querySelector(".teclas-2");
 let resultado = false;
 let valorAtual = "";
 
-var b = true;
+let b = true;
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
@@ -14,16 +14,21 @@ buttons.forEach((button) => {
 
       if (
         resultado &&
-        buttonText != "+" &&
-        buttonText != "-" &&
-        buttonText != "*" &&
-        buttonText != "/" &&
-        buttonText != "." &&
-        buttonText != "%"
+        buttonText !== "+" &&
+        buttonText !== "-" &&
+        buttonText !== "*" &&
+        buttonText !== "/" &&
+        buttonText !== "." &&
+        buttonText !== "%" &&
+        buttonText !== "x\u00B2"
       ) {
         valorAtual = buttonText;
       } else {
-        valorAtual += buttonText;
+        if (buttonText === "x\u00B2") {
+          valorAtual += "\u00B2";
+        } else {
+          valorAtual += buttonText;
+        }
       }
 
       display.value = valorAtual;
@@ -65,20 +70,6 @@ equal_2.addEventListener("click", () => {
   }
 });
 
-const quadrado = document.getElementById("quadrado");
-quadrado.addEventListener("click", () => {
-  if (b == true) {
-    const value = parseFloat(display.value); // Convert display value to a number
-
-    if (!isNaN(value)) {
-      const squaredValue = value * value;
-      display.value = squaredValue;
-      valorAtual = squaredValue;
-      resultado = true;
-    }
-  }
-});
-
 equal.addEventListener("click", () => {
   if (b == true) {
     try {
@@ -94,9 +85,26 @@ equal.addEventListener("click", () => {
   }
 });
 
+const quadrado = document.getElementById("quadrado");
+quadrado.addEventListener("click", () => {
+  if (b == true) {
+    handleQuadradoClick();
+    resultado = true;
+  }
+});
+
 function evaluateExpression(expression) {
   const sanitizedExpression = expression.replace(/[^\d+\-*/().%]/g, "");
   return Function(`'use strict'; return (${sanitizedExpression})`)();
+}
+
+function handleQuadradoClick() {
+  if (b == true) {
+    const value = display.value;
+    const squaredValue = evaluateExpression(`(${value}) * (${value})`);
+    display.value = squaredValue;
+    valorAtual = squaredValue;
+  }
 }
 
 function changeKeys() {
